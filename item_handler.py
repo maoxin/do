7ac740f8f47d462e0655dd6073c8063c583ac54d
maoxin <maoxin.horizon@gmail.com>
2014-04-23 01:43:19 +0800

    motor update about to list

--- item_handler.py
+++ item_handler.py
@@ -528,7 +528,6 @@ class ItemTalkHandler(BaseHandler):
 class ItemGetTalkHandler(BaseHandler):
     def func_after_find_talks(self, result, info):
         if result:
-            print "A"
             latest_id = info
         
             talks = filter(lambda x: x['_id'] > latest_id, result)
