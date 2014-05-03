f68f18a106ecca533bb813fa12e2f17e21a420bf
maoxin <maoxin.horizon@gmail.com>
2014-05-03 22:42:13 +0800

    user id/key identify

--- base_handler.py
+++ base_handler.py
@@ -1,6 +1,7 @@
 import motor
 import tornado.web
 import db_handler
+from dateutil import parser
 
 class BaseHandler(tornado.web.RequestHandler):
     
@@ -16,4 +17,35 @@ class BaseHandler(tornado.web.RequestHandler):
             'email': email,
         }
         
-        collection.do_find_one(query, func_after_check_id, {})
+        collection.do_find_one(query, self.check_id_key_timestamp, {})
+        
+    def check_id_key_timestamp(self, result, info):
+        if result:
+        
+            func_after_check_id = info
+        
+            old_time = parser.parse(result['time'])
+            later_time = datetime.now()
+            
+            delta = later_time - old_time
+            if (delta.days == 0 and delta.seconds < 3600):
+                func_after_check_id(result, {})
+            else:
+                message = {"response": "id_key_expired"}
+                message_json = json.dumps(message)
+                self.set_header("Content_Type", "application/json")
+                self.write(message_json)
+                print 'connected fail'
+        
+                self.finish()
+                return
+        
+        else:
+            message = {"response": "fail"}
+            message_json = json.dumps(message)
+            self.set_header("Content_Type", "application/json")
+            self.write(message_json)
+            print 'connected fail'
+        
+            self.finish()
+            return
