d876261ecd288568a02a45a920c8ee4e15f68046
maoxin <maoxin.horizon@gmail.com>
2014-05-05 00:35:38 +0800

    map web socket

--- web_socket_handler.py
+++ web_socket_handler.py
@@ -157,7 +157,6 @@ class MapWebSocket(tornado.websocket.WebSocketHandler):
                 
                 self.write_message(info)
                 print 'map connect succeed'
-                self.finish()
             
             except KeyError:
                 self.write_message( {'status': 'error', 'detail': 'enter the right key'})
